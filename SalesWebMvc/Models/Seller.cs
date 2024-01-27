26c363a9c43e82a1421357c83ecd6daf1403fc45
virgiliobreda <virgiliobred031@gmail.com>
2024-01-26 21:21:07 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -9,12 +9,19 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} required")] 
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Enter a valid {0}")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [Required]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
+        [Required]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
